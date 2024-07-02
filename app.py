b2843831a39daa4f0a5a7bb8cf6ba8fcef032e0a
Satyajit Samal <sikunsamal465@gmail.com>
2024-07-02 11:08:46 +0530

    right

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from flask import Flask,render_template,request
-import sqlite3
-import json
-import pickle
+
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("home.html")
+
+@app.route('/recommend')
+def recommend():
+    return render_template("recommend.html")
+
+if __name__ =='__main__':
+    app.run(host ="0.0.0.0",port = 6500)
